5f7761df22429f19707e55b3a44635b867445fc5
Jeffkent01coder <geofreyerastus956@gmail.com>
2021-10-19 17:48:44 +0300

    intial

--- app/src/main/java/com/jeff/eldorhubdeliveries/PaymentMethod.kt
+++ app/src/main/java/com/jeff/eldorhubdeliveries/PaymentMethod.kt
@@ -2,7 +2,10 @@ package com.jeff.eldorhubdeliveries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.jeff.eldorhubdeliveries.databinding.ActivityPaymentMethodBinding
+import com.jeff.eldorhubdeliveries.databinding.EditProfileBinding
+import com.jeff.eldorhubdeliveries.databinding.LipaBinding
 
 class PaymentMethod : AppCompatActivity() {
      private lateinit var binding: ActivityPaymentMethodBinding
@@ -11,5 +14,18 @@ class PaymentMethod : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(binding.root)
+
+        binding.ivMpesa.setOnClickListener {
+            val dialogView = LipaBinding.inflate(layoutInflater)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(dialogView.root)
+                .setTitle("Lipa Na MPESA")
+
+            mBuilder.create().show()
+
+            dialogView.btnCancel3.setOnClickListener {
+                mBuilder.create().cancel()
+            }
+        }
     }
 }
